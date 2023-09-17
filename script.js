1cd58a23c49acdcebd9f4f323df27043c19265d7
alyamannai <alyamannaimat@gmail.com>
2023-09-17 20:43:15 +0400

    added button prompts user for grid size

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let btn = document.getElementById('gridbutton');
+    btn = prompt('enter grid size 1-64');
+
+    if (btn>= 1 && btn<=64){
+        container = this.getElementsById('container');
+        container = "";
+
+        
+
+
+    }
+});
